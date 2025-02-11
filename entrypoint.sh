075b1220d1027a184b62e960893432d0491ac4d1
Alejandro Irrazabal <>
2025-02-11 18:38:28 -0300

    Imprimimos una línea en blanco con el titulo para mejorar la legibilidad en los logs en entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,9 @@ HOST=$3
 PORT=$4
 COMMANDS=$5
 
+# Imprimimos una línea en blanco con el titulo para mejorar la legibilidad en los logs
+echo -e "\n\n--- Iniciando conexión SSH ---\n"
+
 # Realizamos la conexión SSH y ejecutamos los comandos
 sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no -o LogLevel=quiet -p "$PORT" "$USER@$HOST" << EOF
   $COMMANDS
